e5053a635d8a799a823d0da99eaceee4366e174c
olearykieran <koleary515@gmail.com>
2025-03-31 17:45:27 -0400

    favicon

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 180,
+  height: 180,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 140,
+          background: "#000000",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          fontFamily: "system-ui",
+        }}
+      >
+        N
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
